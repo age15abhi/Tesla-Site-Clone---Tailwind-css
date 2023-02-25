44ddb06f12ea3bc56ad8dbe43f3ac343c46a554b
Abhishek <abhi905040@gamil.com>
2023-02-26 03:04:51 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: { 
+      colors: {
+        "tesla-dark": "#393c41",
+      },
+fontFamily: {
+  
+}
+  
+    },
+  },
+  plugins: [],
+}
